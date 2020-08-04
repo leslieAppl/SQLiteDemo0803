422085c530c870fd42fa10123d6d3760b416cdfe
Leslie <leslie.appl@gmail.com>
2020-08-04 13:58:25 +0800

    Creating the Database and Table

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 
 ## Built UI
 
+## Creating the Database and Table
+
+
+
--- SQLiteDemo0803/ViewController.swift
+++ SQLiteDemo0803/ViewController.swift
@@ -19,9 +19,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initDB()
     }
 
+    //MARK: - Creating the Database and Table
+    func initDB() {
+        let filemgr = FileManager.default
+        let dirPaths = filemgr.urls(for: .documentDirectory, in: .userDomainMask)
+        databasePath = dirPaths[0].appendingPathComponent("contacts.db").path
+        
+        if !filemgr.fileExists(atPath: databasePath) {
+            
+            let contactDB = FMDatabase(path: databasePath)
+            
+            if contactDB.open() {
+                ///FMDB uses strings for all your SQL commands (CREATE, INSERT, DELETE, etc.).
+                let sql_stmt = "CREATE TABLE IF NOT EXISTS CONTACTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ADDRESS TEXT, PHONE TEXT)"
+                ///This executes a series of SQL statements
+                if !contactDB.executeStatements(sql_stmt) {
+                    print("Error: \(contactDB.lastErrorMessage())")
+                }
+                
+                ///After finished manipulating DB, close it.
+                contactDB.close()
+            } else {
+                print("Error: \(contactDB.lastErrorMessage())")
+            }
+        }
+    }
+    
     @IBAction func saveBtnPressed(_ sender: UIButton) {
         
     }
